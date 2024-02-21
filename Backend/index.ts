926e8f37ffe2eeff3ffccc2e371d436fa4c352f7
Marcos_Neiva <103659504+Marcos1701@users.noreply.github.com>
2024-02-21 01:01:26 +0000

    Enviando Api :tada:

--- Backend/index.ts
+++ Backend/index.ts
@@ -1,18 +1,46 @@
 Bun.serve({
-    fetch
+    fetch: fetchApi,
+    port: 3000
 });
 
+const corsHeaders = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "Access-Control-Max-Age": "86400"
+};
 
-function fetch(req: Request) {
+// api => https://api.hgbrasil.com/weather?lat=-23.682&lon=-46.875&key=45e3765d
+// api key => 45e3765d
+
+async function apiRequest(req: Request): Promise<Response>
+{
     const url = new URL(req.url);
-    const routes = {
-        "/": "Home page!",
-        "/blog": "Blog!",
-    };
-    if (url.pathname === "/") return new Response("Home page!");
-    if (url.pathname === "/blog") return new Response("Blog!");
-    return new Response(`Rotas disponíveis: ${Object.keys(routes).join(", ")}`);
+    const apiKey = "45e3765d";
+    const api = "https://api.hgbrasil.com/weather";
+    const lat = url.searchParams.get("lat");
+    const lon = url.searchParams.get("lon");
+    const apiUrl = `${api}?lat=${lat}&lon=${lon}&key=${apiKey}`;
+    
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    return new Response(
+        JSON.stringify(data),
+        {
+            headers: {
+                "Content-Type": "application/json",
+                ...corsHeaders
+            }
+        }
+    );
 }
 
+async function fetchApi(req: Request): Promise<Response>
+{
+    const url = new URL(req.url);
+    if (url.pathname === "/") return new Response("Seja Bem vindo a API!", { status: 200 });
+    if (url.pathname === "/api") return await apiRequest(req);
+
+    return new Response("Not found!", { status: 404 });
+}
 
 console.log("Server is running on http://localhost:3000");
